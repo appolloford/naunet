66cdf22fe5399487add1af2731acbadb5167806c
Chia-Jung Hsu <appolloford@gmail.com>
2021-05-28 14:49:38 +0200

    Fix ubuntu CI

--- scripts/actions/build_examples.sh
+++ scripts/actions/build_examples.sh
@@ -1,28 +1,28 @@
 cd $HOME/naunet_example0
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release \
-      -DSUNDIALS_DIR=/usr/local/sundials/lib/cmake/sundials \ 
-      -DCMAKE_INSTALL_PREFIX=./
+      -DSUNDIALS_DIR=/usr/local/sundials/lib/cmake/sundials
+      # -DCMAKE_INSTALL_PREFIX=./ # not work on github workflow
 cmake --build build
-cd build && make install
+# cd build && make install
 
 cd $HOME/naunet_example1
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release \
-      -DSUNDIALS_DIR=/usr/local/sundials/lib/cmake/sundials \
-      -DCMAKE_INSTALL_PREFIX=./
+      -DSUNDIALS_DIR=/usr/local/sundials/lib/cmake/sundials
+      # -DCMAKE_INSTALL_PREFIX=./ # not work on github workflow
 cmake --build build
-cd build && make install
+# cd build && make install
 
 cd $HOME/naunet_example2
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release \
       -DSUNDIALS_DIR=/usr/local/sundials/lib/cmake/sundials \
-      -DCMAKE_CUDA_ARCHITECTURES=61 \
-      -DCMAKE_INSTALL_PREFIX=./
+      -DCMAKE_CUDA_ARCHITECTURES=61
+      # -DCMAKE_INSTALL_PREFIX=./ # not work on github workflow
 cmake --build build
-cd build && make install
+# cd build && make install
 
 cd $HOME/naunet_example3
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release \
-      -DBOOST_ROOT=/usr/local \
-      -DCMAKE_INSTALL_PREFIX=./
+      -DBOOST_ROOT=/usr/local
+      # -DCMAKE_INSTALL_PREFIX=./ # not work on github workflow
 cmake --build build
-cd build && make install
+# cd build && make install
