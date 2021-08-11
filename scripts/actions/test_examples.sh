4e431126009bac4bc2241cf46ea8e42dc457c777
Chia-Jung Hsu <appolloford@gmail.com>
2021-08-11 16:30:41 +0200

    Reduce tests in CI

--- scripts/actions/test_examples.sh
+++ scripts/actions/test_examples.sh
@@ -1,11 +1,11 @@
 cd $HOME/naunet_example0/build
-ctest --output-on-failure
+ctest -R "single|pymodule" -j4 --output-on-failure
 
 cd $HOME/naunet_example1/build
-ctest --output-on-failure
+ctest -R "single|pymodule" -j4 --output-on-failure
 
 cd $HOME/naunet_example2/build
-ctest --output-on-failure
+ctest -R "single|pymodule" -j4 --output-on-failure
 
 cd $HOME/naunet_example3/build
-ctest --output-on-failure
+ctest -R "single|pymodule" -j4 --output-on-failure
