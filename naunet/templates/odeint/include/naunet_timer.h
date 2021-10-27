0a89750b41c18e52752966e95f38bc5c1330b677
Chia-Jung Hsu <appolloford@gmail.com>
2021-10-27 23:44:05 +0200

    Try using clang-format in odeint templates

--- naunet/templates/odeint/include/naunet_timer.h
+++ naunet/templates/odeint/include/naunet_timer.h
@@ -3,37 +3,32 @@
 
 #include <chrono>
 
-class Timer
-{
-private:
+class Timer {
+   private:
     bool m_status;
     std::chrono::time_point<std::chrono::high_resolution_clock> m_start;
     std::chrono::time_point<std::chrono::high_resolution_clock> m_end;
     std::chrono::duration<double> m_duration;
 
-public:
-    void start()
-    {
-        m_start = std::chrono::high_resolution_clock::now();
+   public:
+    void start() {
+        m_start  = std::chrono::high_resolution_clock::now();
         m_status = true;
     };
-    void stop()
-    {
-        m_end = std::chrono::high_resolution_clock::now();
+    void stop() {
+        m_end    = std::chrono::high_resolution_clock::now();
         m_status = false;
     };
-    void restart()
-    {
-        m_start = std::chrono::high_resolution_clock::now();
+    void restart() {
+        m_start  = std::chrono::high_resolution_clock::now();
         m_status = false;
     }
-    double elapsed()
-    {
-        // return std::chrono::duration_cast<std::chrono::microseconds>(m_end-m_start).count();
+    double elapsed() {
+        // return
+        // std::chrono::duration_cast<std::chrono::microseconds>(m_end-m_start).count();
         m_duration = m_end - m_start;
         return m_duration.count();
     };
 };
 
 #endif
-
