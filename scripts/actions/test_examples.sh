f032aad90d9c39e1e8f6b2f6a0db4e0a1fa2d61e
Chia-Jung Hsu <appolloford@gmail.com>
2023-12-30 21:39:52 +0100

    Bump cuda version

--- scripts/actions/test_examples.sh
+++ scripts/actions/test_examples.sh
@@ -3,10 +3,10 @@ do
   if [[ "2 6 10 14" =~ (^|[[:space:]])"$idx"($|[[:space:]]) ]]; then
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
       cd $HOME/naunet_example$idx/build
-      ctest -R "single|pymodule" -j4 --output-on-failure
+      ctest -R "single|pymodule" -j4 -V --output-on-failure
     fi
   else
     cd $HOME/naunet_example$idx/build
-    ctest -R "single|pymodule" -j4 --output-on-failure
+    ctest -R "single|pymodule" -j4 -V --output-on-failure
   fi
 done
